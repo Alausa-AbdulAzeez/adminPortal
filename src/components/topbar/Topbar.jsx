e78dbc9146a1b45905b9bc49c86f64df7997e708
kenzo7664 <olaotanemmanuel1@gmail.com>
2023-01-09 11:21:28 +0100

    added topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,18 @@
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import './topbar.scss'
 
 const Topbar = () => {
-  return <div>Topbar</div>
+  return (
+    <div className='topbarWrapper'>
+      <div className='' data-testid='loggedInUser'>
+        Welcome Sophie
+      </div>
+
+      <FontAwesomeIcon icon={faUser} className='topbarUserIcon' />
+    </div>
+  )
 }
 
 export default Topbar
--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
 import './homepage.scss'
 
 const Homepage = () => {
-  return <div className='main'></div>
+  return (
+    <div className='main'>
+      <Topbar />
+    </div>
+  )
 }
 
 export default Homepage
