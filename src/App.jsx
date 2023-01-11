f1ac396ffa22cd5b945fc9f7652a8a57de1be091
kenzo7664 <alausaabdulazeez@gmail.com>
2023-01-11 12:47:19 +0100

    created claim properties container

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/sidebar/Sidebar'
 import Homepage from './pages/home/Homepage'
+import SingleClaim from './pages/singleClaim/SingleClaim'
 import SinglePile from './pages/singlePile/SinglePile'
 import VetClaims from './pages/vetClaims/VetClaims'
 
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Homepage />} />
           <Route path='/vetclaims' element={<VetClaims />} />
           <Route path='/singlepile' element={<SinglePile />} />
+          <Route path='/viewclaim' element={<SingleClaim />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/singleClaim/SingleClaim.jsx
+++ src/pages/singleClaim/SingleClaim.jsx
@@ -1,7 +1,84 @@
 import './singleClaim.scss'
+import Topbar from '../../components/topbar/Topbar'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { maxWidth } from '@mui/system'
 
 const SingleClaim = () => {
-  return <div>SingleClaim</div>
+  return (
+    <div className='singleClaimMain'>
+      <Topbar title='Claim Info' />
+      <div className='singleClaimContentWrapper'>
+        <div className='claimActionsWrapper'>
+          <div className='claimActionLeft'>
+            <button className='claimActionBtn previousBtn'>Previous</button>
+            <button className='claimActionBtn nextBtn'>Next</button>
+          </div>
+          <div className='claimActionCenter'>Claim: 1 of 1 in pile</div>
+          <div className='claimActionRight'>
+            <button className='claimActionBtn approveClaimBtn'>Approve</button>
+            <button className='claimActionBtn rejectBtn'>Reject</button>
+          </div>
+        </div>
+        <Accordion sx={{ width: maxWidth, marginBottom: 5 }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls='panel1a-content'
+            id='panel1a-header'
+          >
+            <Typography>Claim Properties</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className='claimPropertiesWrapper'>
+              <div className='claimPropertiesLeft claimProperties'>
+                <ul>
+                  <li>
+                    Name: <span>Alausa</span>
+                  </li>
+                  <li>
+                    Encounter Date:
+                    <span>{Date().toString().slice(0, 16)}</span>
+                  </li>
+                </ul>
+              </div>
+              <div className='claimPropertiesCenter claimProperties'>
+                <ul>
+                  <li>
+                    Diagnosis: <span>Malaria</span>
+                  </li>
+                  <li>
+                    Probider:{' '}
+                    <span>
+                      Topaz Clinic Lorem ipsum dolor sit amet, consectetur
+                      adipisicing elit.
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              <div className='claimPropertiesRight claimProperties'>
+                <ul>
+                  <li>
+                    Billed: <span>N100,000</span>
+                  </li>
+                  <li>
+                    Payable:<span>N100,000</span>
+                  </li>
+                  <li>
+                    Status: <span>Pending</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  )
 }
 
 export default SingleClaim
